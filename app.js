b2bbb40f4973627e1220eeb2335fcea2d61b3519
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-12-15 16:25:56 +0500

    'practice'

---
+++ app.js
@@ -0,0 +1,11 @@
+var names = 'haseeb'
+let email = 'haseeb@gmail.com'
+console.table([names,email])
+
+
+function test() {
+    var names = 'adil'
+    let email = 'adil@gmail.com'
+    console.table([names,email])
+}
+test()
