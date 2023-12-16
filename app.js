c761e846e9c2d66aedeb155049c45b9a55c102f5
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-12-16 16:15:07 +0500

    data type practice

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-var names = 'haseeb'
-let email = 'haseeb@gmail.com'
-console.table([names,email])
+// var names = 'haseeb'
+// let email = 'haseeb@gmail.com'
+// console.table([names,email])
 
 
-function test() {
-    var names = 'adil'
-    let email = 'adil@gmail.com'
-    console.table([names,email])
-}
-test()
+// function test() {
+//     var names = 'adil'
+//     let email = 'adil@gmail.com'
+//     console.table([names,email])
+// }
+// test()
+
+
+// function checkNull(value) {
+//     if (value === null) {
+//       console.log("The value is null.");
+//     } else {
+//       console.log("The value is not null. It is:", value);
+//     }
+//   }
+
+// Example usage:
+//   let example1 = null;
+//   let example2 = "Hello, World!";
+//   let example3 = 42;
+
+//   checkNull(example1); // Output: The value is null.
+//   checkNull(example2); // Output: The value is not null. It is: Hello, World!
+//   checkNull(example3); // Output: The value is not null. It is: 42
+
+
+//   function haseeb(h) {
+//     if (h === null) {
+//         console.log('null');
+//     } else {
+//         console.log(h);
+//     }
+//   }
+
+//   let adil = null
+//   let adil1 = 39
+
+//   haseeb(adil)
+//   haseeb(adil1 )
+
+
+// let score = null 
+
+// let num = Number(score)
+// console.log(num);
+// console.log(typeof num);
+
+let boolean = 'e'
+
+let boo = Boolean(boolean)
+console.log(boo);
+console.log(typeof boo);
+
+
