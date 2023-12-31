3ec12a890d0fd13c4cf41355238219b9a53588e8
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-12-31 16:01:47 +0500

    date method pratice

--- app.js
+++ app.js
@@ -50,10 +50,149 @@
 // console.log(num);
 // console.log(typeof num);
 
-let boolean = 'e'
+// let boolean = 'e'
 
-let boo = Boolean(boolean)
-console.log(boo);
-console.log(typeof boo);
+// let boo = Boolean(boolean)
+// console.log(boo);
+// console.log(typeof boo);
 
 
+// let id = Symbol('123')
+// let anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// console.log(id);
+// console.log(anotherId);
+
+
+// let arr =
+// console.log(typeof arr);
+
+// let num = 22 
+// let num2 = num
+// num2 = 33
+
+// console.log(num2);
+// console.log(num);
+
+// let num = true 
+// let num2 = num
+// num2 = false
+
+// console.log(num2);
+// console.log(num);
+
+// let userOne = {
+//     names: 'haseeb',
+//     age: 2
+// }
+
+// let userTwo = userOne
+// userTwo.names = 'adil'
+
+// console.log(userTwo);
+// console.log(userOne);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// const balance = new Number(100000)
+// console.log(balance);
+// console.log(balance.toExponential());
+// console.log(balance.toFixed());
+
+// console.log(balance.toString().slice(0,5));
+// console.log(balance.toLocaleString('en-pk'));
+// let balance = 1000000;
+// console.log(balance.toLocaleString('en-In'));
+
+
+// let names = new String('haseeb')
+// console.log(names.includes());
+// console.log(names.slice(0,5));
+// console.log(names.split());
+// console.log(names.toLocaleUpperCase());
+// console.log(names.toLocaleUpperCase());
+
+
+
+// const myArray = [1, 2, 3, 4, 5];
+// const element = 7;
+
+// if (myArray.includes(element)) {
+//   console.log("The array contains the element.");
+// } else {
+//   console.log("The array does not contain the element.");
+// }
+
+
+// const myString = "Hello, World!";
+// const substring = "hello";
+
+// if (myString.toLowerCase().includes(substring)) {
+//   console.log("The string contains the substring (case-insensitive).");
+// } else {
+//   console.log("The string does not contain the substring.");
+// }
+
+// let arr = [1, 2, 3, 4, 5,]
+// let element = 'haseeb'
+
+// if (arr.includes(element)) {
+//     console.log('true');
+// } else {
+//     console.log('False');
+
+// }
+
+// Exercise: Check if a specific word is present in a sentence
+
+// Step 1: Create a function named checkWordInSentence
+// The function should take two parameters: sentence and word
+
+// function checkWordInSentence(sentence, word) {
+//     // Step 2: Use the .includes() method to check if the word is present in the sentence
+//     // Note: Make sure the check is case-insensitive
+  
+//     // Step 3: Log a message to the console based on whether the word is found or not
+//     if (sentence.toLowerCase().includes(word.toLowerCase())) {
+//       console.log(`"${word}" is present in the sentence.`);
+//     } else {
+//       console.log(`"${word}" is not found in the sentence.`);
+//     }
+//   }
+  
+//   // Step 4: Test the function with different sentences and words
+  
+//   checkWordInSentence("The quick brown fox jumps over the lazy dog", "fox");
+//   checkWordInSentence("JavaScript is a powerful programming language", "Python");
+//   checkWordInSentence("Hello, World!", "world");
+// function checkWordInSentence(sentence, word) {
+//     if (sentence.toLowerCase().includes(word.toLowerCase())) {
+//         console.log(`${word} present in the sentence`);
+//     } else {
+//         console.log(`${word} is not found in the sentence`);
+        
+//     }
+    
+// }
+
+// checkWordInSentence("My name is Abdul Haseeb", "haseeb")
+
+// const myDate = new Date()
+// console.log(myDate);
+// console.log('date', myDate.getDate());
+// console.log('day',myDate.getDay());
+// console.log('year',myDate.getFullYear());
+// console.log('hour',myDate.getHours());
+// console.log('month',myDate.getMonth());
+// console.log('min',myDate.getMinutes());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+
+
+  
